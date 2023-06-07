bf81bc905498dc728048bb4aeef17309e34e3db2
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2023-06-07 20:50:51 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,29 @@
+const calc =  ()=> {
+    let op = document.querySelector("#operation").value;
+    let x = document.querySelector("#n1").value;
+    let y = document.querySelector("#n2").value;
+
+    if(x === '' || y === ''){
+        alert("One or More Fields are empty !!!!!!");
+        return;
+    }
+    
+    x = parseFloat(x);
+    y = parseFloat(y);
+    let z = 0.0;
+
+    if(op === 'add'){
+        z = x + y;
+    }
+    else if(op === 'sub'){
+        z = x - y;
+    }
+    else if(op === 'mul'){
+        z = x * y;
+    }
+    else if(op === 'div'){
+        z = x / y;
+    }
+
+    document.querySelector("#result").innerHTML =`Result value is : ${Math.round(z)}`;
+}
